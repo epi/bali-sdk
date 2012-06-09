bc55984c5436af84eee181ac02ce8eb1d758a4c8
Adrian Matoga <epi@atari8.info>
2012-06-09 18:58:55 +0100

    Project renamed.

--- scripts/broker.py
+++ scripts/broker.py
@@ -3,18 +3,18 @@
 # broker - program to install apps on smartphones with bada
 # Copyright (C) 2012 Adrian Matoga
 #
-# bada-sdk-linux is free software: you can redistribute it and/or modify
+# bali-sdk is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 # 
-# bada-sdk-linux is distributed in the hope that it will be useful,
+# bali-sdk is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with bada-sdk-linux.  If not, see <http://www.gnu.org/licenses/>.
+# along with bali-sdk.  If not, see <http://www.gnu.org/licenses/>.
 
 import serial
 import string
